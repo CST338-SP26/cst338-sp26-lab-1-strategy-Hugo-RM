ebbd33d48c26097f8853fdaeceb471e15f7d85ae
Hugo M. Ruiz-Mireles <hugo.ruizmireles@gmail.com>
2026-03-01 23:39:44 -0800

    fixed extend missing

--- src/main/java/Kobold.java
+++ src/main/java/Kobold.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 
-public class Kobold {
+public class Kobold extends Monster {
     public Kobold(Integer maxHp, Integer xp, HashMap<String, Integer> items) {
         super(maxHp, xp, items);
     }
